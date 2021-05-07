b0936d87e8c36cfcbee28657c0b901962a6d9697
Felix Ochoa <fdavidochoa@unicesar.edu.co>
2021-05-07 17:45:35 -0500

    Actualizacion Citas

--- manage-citas.php
+++ manage-citas.php
@@ -2,7 +2,57 @@
 include("sidebar-adm.php");
 ?>
 
+<div class="container w-full md:w-4/5 xl:w-3/5 px-2">
 
+<h2>
+    Personal de atención
+</h2>
+    <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+        <table id="ListaPersonal" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+            <thead>
+                <tr>
+                    <th>Foto</th>
+                    <th>Nombre de Usuario</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Tipo Usuario</th>
+                    <th>Estado</th>
+                    <th>Trabajando</th>
+                    <th>Gestión</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+<div class="container w-full md:w-4/5 xl:w-3/5 px-2">
+<h2>
+    Pacientes
+</h2>
+    <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+        <table id="ListaPaciente" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+            <thead>
+                <tr>
+                    <th>Foto</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Fecha de nacimiento</th>
+                    <th>Edad</th>
+                    <th>Direccion</th>
+                    <th>Barrio</th>
+                    <th>Telefono</th>
+                    <th>Ciudad</th>
+                    <th>Estado</th>
+                    <th>Gestión</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
 
 </div>
 <?php
