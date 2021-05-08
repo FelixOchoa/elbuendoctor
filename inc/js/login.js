9f0a6f0973133633368455c77a973b4068e624a3
Felix Ochoa <fdavidochoa@unicesar.edu.co>
2021-05-08 01:44:36 -0500

    Login Mejorado

--- inc/js/login.js
+++ inc/js/login.js
@@ -13,16 +13,50 @@ form.addEventListener('submit', function(e){
             let resp = eval("(" + http.responseText + ")");
             if(resp['response'] == 'bien'){
                 if(resp['cargo'] == 'administrador'){
-                    window.location.href = "homepage-adm.php";
+                    bienLogin();
+                    setTimeout(function(){ window.location.href = "homepage-adm.php"; }, 1500);
+                    
                 }
                 else{
-                    window.location.href = "homepage-per.php";
+                    bienLogin();
+                    setTimeout(function(){ window.location.href = "homepage-per.php"; }, 1500);
+                    
                 }
             }
             else{
-                console.log("mal");
+                errorLogin();
             }
         }
     }
     http.send(data);
 })
+
+function bienLogin() {
+    Swal.fire(
+        {
+            title: 'Correcto: Iniciaste sesión correctamente!',
+            padding: '1rem',
+            showConfirmButton: false,
+            toast: true,
+            icon: 'success',
+            grow: 'row',
+            position: 'bottom',
+            width: '55%',
+        }
+    );
+}
+
+function errorLogin() {
+    Swal.fire(
+        {
+            title: 'Error: No se pudo iniciar sesión, intentalo nuevamente',
+            padding: '1rem',
+            showConfirmButton: false,
+            toast: true,
+            icon: 'error',
+            grow: 'row',
+            position: 'bottom',
+            width: '55%',
+        }
+    );
+}
